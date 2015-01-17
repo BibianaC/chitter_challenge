d17d5fe8bd329a0465aeddb18b6cde999596d5b1
bibiana <bcristofol@gmail.com>
2015-01-17 12:00:26 +0000

    Extracted datamapper related code from server to data_mapper_setup file

--- app/server.rb
+++ app/server.rb
@@ -4,14 +4,7 @@ require 'data_mapper'
 require './app/models/peep' 
 require './app/models/user'
 
-
-env = ENV['RACK_ENV'] || 'development'
-
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
